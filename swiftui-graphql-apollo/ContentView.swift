7884971905a29a96c905157f10d77ca853a92a74
Felipe Augusto Silva <felipe.asilva.ext@panonline.com.br>
2024-01-09 09:24:25 -0300

    Initial Commit

---
+++ swiftui-graphql-apollo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-graphql-apollo
+//
+//  Created by Felipe Augusto Silva on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-graphql-apollo/swiftui_graphql_apolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_graphql_apolloApp.swift
+//  swiftui-graphql-apollo
+//
+//  Created by Felipe Augusto Silva on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_graphql_apolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
